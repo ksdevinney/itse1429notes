a8a857684d746a9eb5fd05626336ae4fffcdf8af
ksdevinney <katie@devinney.org>
2022-02-10 21:23:52 -0600

    lab 3a

---
+++ labs/program3a2.py
@@ -0,0 +1,21 @@
+# Program 3A2
+# Katie Devinney
+# Calculates the sum of all numbers before an input number, and averages them
+
+selectedNumber = int(input("Enter a number: "))
+sum = 0
+inBetween = 0
+average = 0
+
+if selectedNumber < 0: # get positive number
+    selectedNumber = abs(selectedNumber)
+elif selectedNumber == 0: # get a nonzero number
+    electedNumber = int(input("Enter a number: "))
+
+for count in range(1, selectedNumber + 1):
+    sum += count
+    inBetween += 1
+
+average = sum / inBetween
+
+print("sum: %-6d" % sum + "numbers between: %-6d" % inBetween + "average: %-6.2f" % average)
---
+++ labs/program3a3.py
@@ -0,0 +1,10 @@
+# Program 3A3
+# Katie Devinney
+# determines if a number is even or odd
+
+for numbers in range(5):
+    inputNumber = int(input("Enter a number: "))
+    if inputNumber % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
