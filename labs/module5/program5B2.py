3975b696e4e9dd3db86c2be0ca44dcff0f24cf88
ksdevinney <katie@devinney.org>
2022-03-16 15:54:21 -0500

    program 5b2

--- labs/module5/program5B2.py
+++ labs/module5/program5B2.py
@@ -1,4 +1,33 @@
 # Program 5B2
 # Katie Devinney
-#
+# determines if the user inputs an acceptable variable name
 
+file = open("labs/module5/Lab5B2.txt", 'r')
+
+words = file.read()
+words = words.split()
+
+reservedWords = tuple(words)
+
+print("You are not allowed to use these words:\n", reservedWords)
+
+variable = input("Enter a variable name: ")
+acceptable = True
+
+if variable[0].isalpha() == False:
+    print("Variable names must start with a letter.")
+    acceptable = False
+
+for character in range(len(variable)):
+    if variable[character].isalpha() == False and variable[character].isnumeric() == False and not variable[character] == '_':
+        print("Variable names can only contain letters, numbers, and _.")
+        acceptable = False
+
+if variable in reservedWords:
+    print("Variable cannot use words from the list of reserved words.")
+    acceptable = False
+
+if acceptable:
+    print("Your variable name is valid!")
+else:
+    print("Your variable name is not valid.")
