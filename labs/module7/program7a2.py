6272b9f04931298f206dc16eea4e5edfd9d99784
ksdevinney <katie@devinney.org>
2022-04-14 21:22:38 -0500

    turtle leaves, comments

--- labs/module7/program7a2.py
+++ labs/module7/program7a2.py
@@ -7,24 +7,35 @@ from turtle import Turtle
 def main():
     buster = Turtle()
 
-    row = 4
-    indent = 0
+    setPins(buster, 4)
 
+    # get out of the way!
+    buster.up()
+    buster.goto(300, 300)
+
+# draw the dots
+def setPins(t, row):
     startX = 0
     startY = 0
+    indent = 0
 
+    # iterate over each row
     while row > 0:
+        # set pins in the row
         for pin in range(row):
-            buster.dot(20)
-            buster.up()
-            buster.forward(50)
+            t.dot(20)
+            t.up()
+            t.forward(50)
+        
+        # each row is shorter than the last
         row -= 1
+       
+        # each row gets closer to the middle
         indent += 1
-        buster.up()
-        buster.goto(startX + 25 * indent , startY - indent * 50)
-
-    
-
+        t.up()
+        
+        # move to the next row
+        t.goto(startX + 25 * indent , startY - indent * 50)
 
 if __name__ == "__main__":
     main()
