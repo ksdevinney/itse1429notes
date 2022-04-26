7a2b9e3ab3eb1832e68a7a2c79f4fca5d24f7983
ksdevinney <katie@devinney.org>
2022-04-26 10:06:19 -0500

    lab 1 good enough

--- labs/module8/lab8b1.py
+++ labs/module8/lab8b1.py
@@ -1,3 +1,34 @@
 # Lab 8B1
 # Katie Devinney
-# Converts a temperature in farenheit to celcius
+# Converts a temperature in farenheit to celsius
+
+from breezypythongui import EasyFrame
+
+class TempConverter(EasyFrame):
+    def __init__(self):
+        EasyFrame.__init__(self)
+        self.addLabel(text = "Enter the temperature, in Farenheit", row = 0, column = 0)
+        self.setTitle("Temperature Converter")
+
+        # input for F
+        self.addLabel(text = "Farenheit", row = 1, column = 0)
+        self.inputField = self.addFloatField(value = 0, row = 1, column = 1)
+
+        # output for C
+        self.addLabel(text = "Celsius", row = 2, column = 0)
+        self.outputField = self.addFloatField(value = 0, row = 2, column = 1, precision = 1)
+
+        # button
+        self.addButton(text = "Convert", row = 3, column = 0, command = self.convert)
+
+    def convert(self):
+        far = self.inputField.getNumber()
+
+        cels = (far - 32) * (5/9)
+        self.outputField.setNumber(cels)
+
+def main():
+    TempConverter().mainloop()
+
+if __name__ == "__main__":
+    main()
